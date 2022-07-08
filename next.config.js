219aa51bc96743919c72f655a71c12cb3bca8a3b
bigthinz <bigthinzcount@gmail.com>
2022-07-08 13:42:14 +0100

    babel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '',
-  // env:{
-  //   DB_CONNECTION: 'mongodb://localhost:27017/kinrabb'
-    // DB_CONNECTION: 'mongodb+srv://dave:cainandabel4o@cluster0.yeihc.mongodb.net/kinrabb?retryWrites=true&w=majority'
-  // },
+
   reactStrictMode: true,
 }
 
